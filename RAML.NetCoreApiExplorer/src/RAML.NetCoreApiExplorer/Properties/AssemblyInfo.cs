fddab7e961a25e4d41552b07e29106cbacdeacd1
Pedro Wood <mail_pedro@yahoo.com.ar>
2016-11-30 15:36:26 -0300

    set version

--- RAML.NetCoreApiExplorer/src/RAML.NetCoreApiExplorer/Properties/AssemblyInfo.cs
+++ RAML.NetCoreApiExplorer/src/RAML.NetCoreApiExplorer/Properties/AssemblyInfo.cs
@@ -5,10 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+[assembly: AssemblyDescription("This package is used by the RAML Tools for .NET Visual Studio extension to generate RAML metadata from an existing ASP.NET Core implementation. In Visual Studio, go to Tools -> Extensions and Updates, then search for RAML in the Online gallery.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MuleSoft Inc")]
 [assembly: AssemblyProduct("RAML.NetCoreApiExplorer")]
 [assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("MuleSoft Inc, Copyright © 2016")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -17,3 +19,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bca5919c-957f-4a75-b75a-bcf72fc9734e")]
+
+[assembly: AssemblyVersion("0.0.1")]

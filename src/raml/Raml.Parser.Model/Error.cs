a05f9c281f38926cbefc3976a4f36eb867da1b72
Pedro Wood <mail_pedro@yahoo.com.ar>
2018-06-01 11:56:58 -0300

    add raml parser model

---
+++ src/raml/Raml.Parser.Model/Error.cs
@@ -0,0 +1,24 @@
+﻿namespace Raml.Parser
+{
+    public class Error
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+        public string Path { get; set; }
+        public bool IsWarning { get; set; }
+        public Position Start { get; set; }
+        public Position End { get; set; }
+    }
+
+    public class Position
+    {
+        public int? Line { get; set; }
+        public int? Col { get; set; }
+        public int? Pos { get; set; }
+
+        public bool HasValue
+        {
+            get { return !(Line == null && Col == null && Pos == null); }
+        }
+    }
+}
---
+++ src/raml/Raml.Parser.Model/Verb.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+
+namespace Raml.Parser.Expressions
+{
+	public class Verb
+	{
+	    public VerbType Type { get; set; }
+
+		public bool IsOptional { get; set; }
+
+		public string Description { get; set; }
+
+	    public IDictionary<string,Parameter> Headers { get; set; }
+
+	    public IEnumerable<Response> Responses { get; set; }
+
+	    public MimeType Body { get; set; }
+
+        public IDictionary<string, Parameter> QueryParameters { get; set; }
+	}
+}

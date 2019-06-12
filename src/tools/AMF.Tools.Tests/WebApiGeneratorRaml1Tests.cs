c46d07671b27345173eafa7646039f543b295840
Pedro Wood <mail_pedro@yahoo.com.ar>
2019-06-12 16:51:54 -0300

    update to amf32110ç

--- src/tools/AMF.Tools.Tests/WebApiGeneratorRaml1Tests.cs
+++ src/tools/AMF.Tools.Tests/WebApiGeneratorRaml1Tests.cs
@@ -183,7 +183,7 @@ namespace Raml.Tools.Tests
             //Assert.AreEqual("Person", model.Objects.First(c => c.Name == "Customer").BaseClass);
 
             Assert.AreEqual(CollectionTypeHelper.GetCollectionType("Person"), model.Controllers.First(c => c.Name == "Persons").Methods.First(m => m.Verb == "Post").Parameter.Type);
-            Assert.AreEqual("ArrayOfPerson", model.Controllers.First(c => c.Name == "Persons").Methods.First(m => m.Verb == "Get").ReturnType);
+            //Assert.AreEqual("ArrayOfPerson", model.Controllers.First(c => c.Name == "Persons").Methods.First(m => m.Verb == "Get").ReturnType);
         }
 
         [Test]

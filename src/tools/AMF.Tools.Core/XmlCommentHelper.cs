bccc98d66f5831a705320c4d2f1255ca2624dbea
Pedro Wood <mail_pedro@yahoo.com.ar>
2019-07-24 16:00:03 -0300

    line end escaping in enums description

--- src/tools/AMF.Tools.Core/XmlCommentHelper.cs
+++ src/tools/AMF.Tools.Core/XmlCommentHelper.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 
 namespace AMF.Tools.Core
 {
@@ -6,10 +7,16 @@ namespace AMF.Tools.Core
     {
         public static string Escape(string text)
         {
+            char unicodeNextLine = '\u0085';
+            char unicodeLineSeparator = '\u2028';
+            char unicodeParagraphSeparator = '\u2029';
             return text.Replace("\r\n", string.Empty)
                 .Replace("\n", string.Empty)
                 .Replace("\r", string.Empty)
                 .Replace(Environment.NewLine, string.Empty)
+                .Replace(unicodeNextLine.ToString(), string.Empty)
+                .Replace(unicodeLineSeparator.ToString(), string.Empty)
+                .Replace(unicodeParagraphSeparator.ToString(), string.Empty)
                 .Replace("&", "&amp;")
                 .Replace("<", "&lt;")
                 .Replace(">", "&gt;")

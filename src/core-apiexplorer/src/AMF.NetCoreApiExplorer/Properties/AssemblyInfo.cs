37545c2f95faa5eb13d7ba1a5d575993ac0e8b78
Pedro Wood <mail_pedro@yahoo.com.ar>
2019-08-01 14:40:14 -0300

    bump versions

--- src/core-apiexplorer/src/AMF.NetCoreApiExplorer/Properties/AssemblyInfo.cs
+++ src/core-apiexplorer/src/AMF.NetCoreApiExplorer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("This package is used by the AMF Tools for .NET Visual Studio extension to generate RAML metadata from an existing ASP.NET Core implementation. In Visual Studio, go to Tools -> Extensions and Updates, then search for AMF in the Online gallery.")]
+[assembly: AssemblyDescription("This package is used by the AMF .Net Tools Visual Studio extension to generate RAML metadata from an existing ASP.NET Core implementation. In Visual Studio, go to Tools -> Extensions and Updates, then search for AMF in the Online gallery.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MuleSoft Inc")]
 [assembly: AssemblyProduct("AMF.NetCoreApiExplorer")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCopyright("MuleSoft Inc, Copyright © 2018")]
+[assembly: AssemblyCopyright("MuleSoft Inc, Copyright © 2019")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bca5919c-957f-4a75-b75a-bcf72fc9734e")]
 
-[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyVersion("0.0.3")]

5fb2298699a76b66da2a949ce3b38144581db333
Pedro Wood <mail_pedro@yahoo.com.ar>
2019-10-11 09:16:14 -0300

    merge core-apicore

---
+++ core-apicore/RAML.NetCore.APICore/src/RAML.NetCore.APICore/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("MuleSoft Inc")]
+[assembly: AssemblyProduct("RAML.NetCore.APICore")]
+[assembly: AssemblyDescription(".Net Core common classes for the RAML tools extension")]
+[assembly: AssemblyCopyright("Copyright © 2016 MuleSoft")]
+
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("206a416f-dd49-4391-bb1f-4466fdb041ba")]
+
+[assembly:AssemblyVersion("0.0.1")]
